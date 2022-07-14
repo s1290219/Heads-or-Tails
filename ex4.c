1b7a003ba8210deca8b0f25c542975f8f66c3421
s1290219 <109235734+s1290219@users.noreply.github.com>
2022-07-14 15:37:04 +0900

    HoT: initial revision

---
+++ ex4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+int i,r,h;
+
+printf("Tossing a coin...\n");
+
+srand((unsigned int)time(NULL));
+
+for(i = 1 ; i < 4 ; i++){
+
+r = (rand()% 2 + 1);
+
+if(r == 1){
+printf("Round %d: Heads\n",i);
+h++;
+}
+else if(r == 2){
+printf("Round %d: Tails\n",i);
+}
+
+}
+
+printf("Heads: %d, Tails: %d\n",h,3-h);
+
+return 0;
+}
